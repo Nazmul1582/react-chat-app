4bd5cd6702291264e6bdb69d5c46af8d9bae5a71
Nazmul1582 <nazmulhasan1582@gmail.com>
2022-11-11 21:49:57 +0600

    initial state of my project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles/App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Input() {
+  return (
+    <div>
+      <h1>Input</h1>
+    </div>
+  );
+}
